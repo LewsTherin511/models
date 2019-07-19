bd865436b84be25dddecf28d99eaa3f230b9a71e
Carlo De Masi <belzagard@gmail.com>
2019-07-19 11:51:38 +0200

    Just messing around with Pycharm management of commit - part02

--- research/object_detection/object_detection_webcam.py
+++ research/object_detection/object_detection_webcam.py
@@ -48,9 +48,6 @@ PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
 
 NUM_CLASSES = 90
 
-print("Printing some really useless random stuff here \n\n!")
-
-
 
 # Download Model and extract
 if not os.path.exists(MODEL_NAME + '/frozen_inference_graph.pb'):
